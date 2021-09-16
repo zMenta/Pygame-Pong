c1b5dca98884c654d1b83eb4815e558bda3f28d1
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-09-15 21:12:30 -0300

    Score system

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 import sys
+from random import choice
 
 # Functions
 def ball_animation():
     global ball_speed_x, ball_speed_y
+    ball_restart()
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -13,14 +15,25 @@ def ball_animation():
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
 
+def ball_start():
+    global ball_speed_x, ball_speed_y
+    ball_speed_x *= -1
+    ball_speed_y *= choice((1, -1))
+
+    ball.x = SCREEN_MIDDLE[0]
+    ball.y = SCREEN_MIDDLE[1]
+
 def ball_restart():
-    global ball_speed_x
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
-        ball.x = SCREEN_MIDDLE[0]
-        ball.y = SCREEN_MIDDLE[1]
+    global opponent_score, player_score
+    if ball.left <= 0:
+        ball_start()
+        player_score += 1
+    if ball.right >= screen_width:
+        ball_start()
+        opponent_score += 1
 
 def opponent_animation():
+    opponent_ai()
     if opponent.top <= 0:
         opponent.top = 0
     if opponent.bottom >= screen_height:
@@ -56,9 +69,8 @@ def opponent_ai():
     if ball.x <= screen_width-400:
         if opponent.top < ball.y:
             opponent.y  += paddle_speed
-        if opponent.bottom - 15 > ball.y:
+        if opponent.bottom > ball.y:
             opponent.y -= paddle_speed
-
 
 # General Setup
 pygame.init()
@@ -85,6 +97,15 @@ paddle_speed = 9
 player_speed = 0
 
 
+# Text 
+player_score = 0
+opponent_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf",32)
+
+
+# Colors
+
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
@@ -97,11 +118,9 @@ while True:
         
 
     ball_animation()
-    ball_restart()
     player_animation()
-    # Opponent
     opponent_animation()
-    opponent_ai()
+    
 
     #Visual
     screen.fill(bg_color)
@@ -110,6 +129,10 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (SCREEN_MIDDLE[0],0), (SCREEN_MIDDLE[0],screen_height))
 
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(player_text, (SCREEN_MIDDLE[0] + 20, 150))
+    screen.blit(opponent_text, (SCREEN_MIDDLE[0] - 30, 150))
 
     # Updating the window
     pygame.display.flip()
